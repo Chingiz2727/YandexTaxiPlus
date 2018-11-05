1186ba6707d4928a4ebde2e27fca7b9b272ae73e
Shyngys Kuandyk <halyqsoft9@gmail.com>
2018-11-05 11:02:09 +0600

    TaxiPlus

---
+++ YandexTaxiPlus/UserInterFace/CityTaxi/CitiesList.swift
@@ -0,0 +1,21 @@
+//
+//  CitiesList.swift
+//  YandexTaxiPlus
+//
+//  Created by Чингиз on 23.10.2018.
+//  Copyright © 2018 Чингиз. All rights reserved.
+//
+
+import Foundation
+class CitiesList {
+    var Name : String?
+    var Region : String?
+    var id : String?
+    var regionId : String?
+}
+class RoadList {
+    var Start : String?
+    var StartId : String?
+    var end : String?
+    var endId : String?
+}
